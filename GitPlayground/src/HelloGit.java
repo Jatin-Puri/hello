5c1ea3ffed35921cef2ede3c1cf1e90b3261b7b6
Training <Training@192.168.252.149>
2020-03-07 14:47:49 +0530

    //changed

--- GitPlayground/src/HelloGit.java
+++ GitPlayground/src/HelloGit.java
@@ -3,6 +3,8 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("hello");
+		System.out.println("hey From Eclipse");
+		System.out.println("i am with stupid admin of github ");
 	}
 
 }
